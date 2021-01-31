86c9b16ef9ea1d135b45034539799163c2ed61ff
duolanda <duolanda@live.cn>
2021-01-31 11:48:29 +0800

    就这样吧
    TMD 烦死了，还是用 colorful 得了，开始封装

--- lurt_color_space.py
+++ lurt_color_space.py
@@ -1,7 +1,8 @@
 import colour
 import numpy as np
 import colorio
-from skimage.color import rgb2xyz, xyz2rgb, rgb2lab
+from skimage.color import rgb2xyz, xyz2rgb, rgb2lab, lab2rgb, rgb2hsv, rgb2ycbcr,rgb2yuv
+from skimage import data, io
 
 def vector_dot(m, v): 
     '''
@@ -12,9 +13,8 @@ def vector_dot(m, v):
 
 srgb_trans = colorio.SrgbLinear()
 
-
-img_in = colour.read_image('test_img/smpte_digital_leader_4k_24fps_2d_ar185_xyz16.tif')
-img_in2 = colour.read_image('test_img/smpte_digital_leader_4k_24fps_2d_ar185_srgb.tif')
+img_in = colour.read_image('test_img/fruits.tif')
+# img_in = io.imread('test_img/fruits.tif')
 
 # img_in = img_in ** (1/2.2)
 
@@ -22,14 +22,19 @@ img_in2 = colour.read_image('test_img/smpte_digital_leader_4k_24fps_2d_ar185_srg
 #     if(i%100 == 0):
 #         print(i/len(img_in)*100)
 #     for j in range(len(img_in[i])):
-#         img_in[i][j] = srgb_trans.from_xyz100(img_in[i][j])
+#         img_in[i][j] = srgb_trans.to_xyz100(img_in[i][j])
 
-# img_out = xyz2rgb(img_in)
 
-# img_out = colour.XYZ_to_sRGB(img_in)
+# img_out = colour.XYZ_to_Lab(colour.sRGB_to_XYZ(img_in))
+# img_out = colour.RGB_to_HSV(img_in)
+# img_out = colour.RGB_to_YCbCr(img_in)
 
-img_out = rgb2lab(img_in2)
+# img_out = rgb2xyz(img_in)
+
 # img_out = img_out ** 2.2
-colour.write_image(img_out, 'output2.bmp')
+# colour.write_image(img_out, 'test_img/output.png')
+
+io.imsave('test_img/output.png', img_out)
+
 
 
