695dcb3e6eaeb61f99e8774e1c2edff0cc4fcfa9
duolanda <duolanda@live.cn>
2021-01-31 13:56:59 +0800

    根据 colorio 改进的 Mysrgb 类

--- lurt_color_space.py
+++ lurt_color_space.py
@@ -9,6 +9,10 @@ def vector_dot(m, v):
     '''
     return np.einsum('...ij,...j->...i', m, v)
 
+def xyy_to_xyz100(xyy):
+    x, y, Y = xyy
+    return np.array([Y / y * x, Y, Y / y * (1 - x - y)]) * 100
+
 def cs_convert(input_cs, out_cs, img, input_gamma = 1.0, output_gamma = 1.0):
     '''
     支持色彩空间：srgb, xyz, lab, hsv, ycbcr
@@ -42,21 +46,34 @@ def cs_convert(input_cs, out_cs, img, input_gamma = 1.0, output_gamma = 1.0):
 
     return img_out
 
-# img_in = colour.read_image('test_img/fruits.tif')
-img_in = colour.read_image('test_img/output.png')
+class MySrgb:
+    def __init__(self):
+        srgb_primaries_xyy = np.array([[0.64, 0.33, 0.212656], [0.30, 0.60, 0.715158], [0.15, 0.06, 0.072186]])
+        srgb_primaries_xyz = xyy_to_xyz100(srgb_primaries_xyy.T)
+        D65 = np.array([95.047, 100, 108.883]) #cie1931
+
+        wp_correction = D65/np.sum(srgb_primaries_xyz, axis=1)
+        self.mat = srgb_primaries_xyz * wp_correction
+        self.mat /= 100
+
+    def xyz_to_srgb(self, xyz):
+        # return np.linalg.solve(self.mat, xyz / 100)
+        self.mat = np.linalg.inv(self.mat)
+        return np.dot(self.mat, xyz)
+
+    def srgb_to_xyz(self, srgb):
+        return 100*np.dot(self.mat, srgb)
 
 
-# for i in range(len(img_in)):
-#     if(i%100 == 0):
-#         print(i/len(img_in)*100)
-#     for j in range(len(img_in[i])):
-#         img_in[i][j] = srgb_trans.to_xyz100(img_in[i][j])
+trans = MySrgb()
+print(trans.xyz_to_srgb(np.array([0.95047, 1.00000, 1.08883])))
 
+# # img_in = colour.read_image('test_img/fruits.tif')
+# img_in = colour.read_image('test_img/output.png')
 
+# img_out = cs_convert('srgb', 'srgb', img_in, input_gamma=2.6, output_gamma=2.2)
 
-img_out = cs_convert('srgb', 'srgb', img_in, input_gamma=2.6, output_gamma=2.2)
-
-colour.write_image(img_out, 'test_img/output2.png')
+# colour.write_image(img_out, 'test_img/output2.png')
 
 
 
