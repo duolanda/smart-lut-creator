cb71af0f6e39a9999b8593b8d473f585e6b39444
duolanda <duolanda@live.cn>
2021-01-31 17:01:06 +0800

    色域转换初步
    下面要整合成函数了

--- lurt_color_space.py
+++ lurt_color_space.py
@@ -9,10 +9,6 @@ def vector_dot(m, v):
     '''
     return np.einsum('...ij,...j->...i', m, v)
 
-def xyy_to_xyz100(xyy):
-    x, y, Y = xyy
-    return np.array([Y / y * x, Y, Y / y * (1 - x - y)]) * 100
-
 def cs_convert(input_cs, out_cs, img, input_gamma = 1.0, output_gamma = 1.0):
     '''
     支持色彩空间：srgb, xyz, lab, hsv, ycbcr
@@ -46,23 +42,6 @@ def cs_convert(input_cs, out_cs, img, input_gamma = 1.0, output_gamma = 1.0):
 
     return img_out
 
-class MySrgb:
-    def __init__(self):
-        srgb_primaries_xyy = np.array([[0.64, 0.33, 0.212656], [0.30, 0.60, 0.715158], [0.15, 0.06, 0.072186]])
-        srgb_primaries_xyz = xyy_to_xyz100(srgb_primaries_xyy.T)
-        D65 = np.array([95.047, 100, 108.883]) #cie1931
-
-        wp_correction = D65/np.sum(srgb_primaries_xyz, axis=1)
-        self.mat = srgb_primaries_xyz * wp_correction
-        self.mat /= 100
-
-    def xyz_to_srgb(self, xyz):
-        # return np.linalg.solve(self.mat, xyz / 100)
-        self.mat = np.linalg.inv(self.mat)
-        return np.dot(self.mat, xyz)
-
-    def srgb_to_xyz(self, srgb):
-        return 100*np.dot(self.mat, srgb)
 
 def colorpy_mat():
     '''
@@ -77,43 +56,41 @@ def colorpy_mat():
         rtn = np.array ([x, y, z])
         return rtn
 
-    SRGB_Red   = xyz_color(0.640, 0.330)
-    SRGB_Green = xyz_color(0.300, 0.600)
-    SRGB_Blue  = xyz_color(0.150, 0.060)
-    SRGB_White = xyz_color(0.95047, 1, 1.08883)  # D65
 
+    SGamut_Red   = xyz_color(0.73, 0.28)
+    SGamut_Green = xyz_color(0.14, 0.855)
+    SGamut_Blue  = xyz_color(0.1, -0.05)
+    D65_White = xyz_color(0.95047, 1, 1.08883)  
 
-    phosphor_red   = SRGB_Red   
-    phosphor_green = SRGB_Green 
-    phosphor_blue  = SRGB_Blue  
-    white_point = SRGB_White 
+    phosphor_red   = SGamut_Red   
+    phosphor_green = SGamut_Green 
+    phosphor_blue  = SGamut_Blue  
+    white_point = D65_White 
 
     phosphor_matrix = np.column_stack ((phosphor_red, phosphor_green, phosphor_blue))
     # Determine intensities of each phosphor by solving:
     #     phosphor_matrix * intensity_vector = white_point
     intensities = np.linalg.solve (phosphor_matrix, white_point)
     # construct xyz_from_rgb matrix from the results
-    xyz_from_rgb_matrix = np.column_stack (
+    specical_to_xyz_matrix = np.column_stack (
         (phosphor_red   * intensities [0],
          phosphor_green * intensities [1],
          phosphor_blue  * intensities [2]))
 
-    # invert to get rgb_from_xyz matrix
-    # rgb_from_xyz_matrix = np.linalg.inv (xyz_from_rgb_matrix)
-    return xyz_from_rgb_matrix
-
-trans = MySrgb()
-print(trans.xyz_to_srgb(np.array([0.95047, 1.00000, 1.08883])))
-rgb_to_xyz_mat = colorpy_mat()
-print(np.dot(np.linalg.inv(rgb_to_xyz_mat), np.array([0.95047, 1.00000, 1.08883])))
+    return specical_to_xyz_matrix
 
 
-# # img_in = colour.read_image('test_img/fruits.tif')
-# img_in = colour.read_image('test_img/output.png')
+sgamut_to_xyz_mat = colorpy_mat()
+xyz_to_sgamut_mat = np.linalg.inv(sgamut_to_xyz_mat)
+
+
+img_in = colour.read_image('test_img/s-log.tif')
 
 # img_out = cs_convert('srgb', 'srgb', img_in, input_gamma=2.6, output_gamma=2.2)
+img_out = vector_dot(sgamut_to_xyz_mat, img_in)
+img_out = cs_convert('xyz', 'srgb', img_out)
 
-# colour.write_image(img_out, 'test_img/output2.png')
+colour.write_image(img_out, 'test_img/output2.png')
 
 
 
