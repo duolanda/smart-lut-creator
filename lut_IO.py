a8adc3d775339c311ccabeb8afda1d7fc23c3034
duolanda <duolanda@live.cn>
2021-02-05 21:31:15 +0800

    预览模块与 IO 完成对接
    pil lut tools 的写法也有可参考之处，所以并没有完全删除，而是先注释，后面再优化到 IO 部分

--- lut_IO.py
+++ lut_IO.py
@@ -1,15 +1,37 @@
 from lut import LUT
-in_lut = LUT.FromCubeFile('test_lut/Lattice_cube3D_Resolve_33.cube')
-# in_lut = LUT.FromNuke3DLFile('test_lut/Lattice_3dl3D_Nuke_32.3dl')
-# in_lut = LUT.FromNuke3DLFile('test_lut/NukeGenerate.3dl')
-# in_lut = LUT.FromLustre3DLFile('test_lut/Lattice_3dl3D_Lustre_17.3dl')
+import numpy as np
 
-print(in_lut.ColorAtLatticePoint(1,0,0))
+def lut_to_numpy(lut):
+    lattice  = lut.lattice
+    size = lut.cubeSize
 
-# in_lut.ToCubeFile('test_lut/smart_cube.cube')
-# in_lut.ToNuke3DLFile('test_lut/smart_3dlNuke.3dl')
-# in_lut.ToLustre3DLFile('test_lut/smart_3dlLustre.3dl')
+    lut_np = np.zeros((size, size, size, 3))
 
-# 当前问题，无法自定义范围，输出的 cube 范围只能是 0~1，Nuke 位深度只有 16，Lustre 只有 12
-# 事实上，输入的时候也没有读 cube 文件的范围
-# 精度也是 numpy 默认的浮点
+    for i in range(size):
+        for j in range(size):
+            for k in range(size):
+                p = lattice[i][j][k]
+                lut_np[i][j][k] = [p.b, p.g, p.r] #注意是bgr
+    return lut_np
+
+
+
+
+if __name__ == '__main__': 
+    in_lut = LUT.FromCubeFile('test_lut/Lattice_cube3D_Resolve_33.cube')
+    # in_lut = LUT.FromNuke3DLFile('test_lut/Lattice_3dl3D_Nuke_32.3dl')
+    # in_lut = LUT.FromNuke3DLFile('test_lut/NukeGenerate.3dl')
+    # in_lut = LUT.FromLustre3DLFile('test_lut/Lattice_3dl3D_Lustre_17.3dl')
+
+    lut_np = lut_to_numpy(in_lut)
+    print(lut_np)
+
+    print(in_lut.ColorAtLatticePoint(1,0,0))
+
+    # in_lut.ToCubeFile('test_lut/smart_cube.cube')
+    # in_lut.ToNuke3DLFile('test_lut/smart_3dlNuke.3dl')
+    # in_lut.ToLustre3DLFile('test_lut/smart_3dlLustre.3dl')
+
+    # 当前问题，无法自定义范围，输出的 cube 范围只能是 0~1，Nuke 位深度只有 16，Lustre 只有 12
+    # 事实上，输入的时候也没有读 cube 文件的范围
+    # 精度也是 numpy 默认的浮点
--- lut_preview.py
+++ lut_preview.py
@@ -4,8 +4,12 @@ from itertools import chain
 
 import numpy as np
 
+from lut import LUT
+from lut_IO import lut_to_numpy
 
-def load_cube_file(lines, target_mode=None, cls=ImageFilter.Color3DLUT):
+
+
+def load_lut(lut, target_mode=None, cls=ImageFilter.Color3DLUT):
     """Loads 3D lookup table from .cube file format.
 
     :param lines: Filename or iterable list of strings with file content.
@@ -14,67 +18,74 @@ def load_cube_file(lines, target_mode=None, cls=ImageFilter.Color3DLUT):
     :param cls: A class which handles the parsed file.
                 Default is ``ImageFilter.Color3DLUT``.
     """
-    name, size = None, None
+    # name, size = None, None
+    # channels = 3
+    # file = None
+
+    # file = lines = open(lines, 'rt')
+
+    # try:
+    #     iterator = iter(lines)
+
+    #     for i, line in enumerate(iterator, 1):
+    #         line = line.strip()
+    #         if line.startswith('TITLE "'):
+    #             name = line.split('"')[1]
+    #             continue
+    #         if line.startswith('LUT_3D_SIZE '):
+    #             size = [int(x) for x in line.split()[1:]]
+    #             if len(size) == 1:
+    #                 size = size[0]
+    #             continue
+    #         if line.startswith('CHANNELS '):
+    #             channels = int(line.split()[1])
+    #         if line.startswith('LUT_1D_SIZE '):
+    #             raise ValueError("1D LUT cube files aren't supported")
+
+    #         try:
+    #             float(line.partition(' ')[0])
+    #         except ValueError:
+    #             pass
+    #         else:
+    #             # Data starts
+    #             break
+
+    #     if size is None:
+    #         raise ValueError('No size found in the file')
+
+    #     table = []
+    #     for i, line in enumerate(chain([line], iterator), i):
+    #         line = line.strip()
+    #         if not line or line.startswith('#'):
+    #             continue
+    #         try:
+    #             pixel = [float(x) for x in line.split()]
+    #         except ValueError:
+    #             raise ValueError("Not a number on line {}".format(i))
+    #         if len(pixel) != channels:
+    #             raise ValueError(
+    #                 "Wrong number of colors on line {}".format(i))
+    #         table.extend(pixel)
+    # finally:
+    #     if file is not None:
+    #         file.close()
+
+    size = lut.cubeSize
+    name = lut.name
     channels = 3
-    file = None
 
-    file = lines = open(lines, 'rt')
-
-    try:
-        iterator = iter(lines)
-
-        for i, line in enumerate(iterator, 1):
-            line = line.strip()
-            if line.startswith('TITLE "'):
-                name = line.split('"')[1]
-                continue
-            if line.startswith('LUT_3D_SIZE '):
-                size = [int(x) for x in line.split()[1:]]
-                if len(size) == 1:
-                    size = size[0]
-                continue
-            if line.startswith('CHANNELS '):
-                channels = int(line.split()[1])
-            if line.startswith('LUT_1D_SIZE '):
-                raise ValueError("1D LUT cube files aren't supported")
-
-            try:
-                float(line.partition(' ')[0])
-            except ValueError:
-                pass
-            else:
-                # Data starts
-                break
-
-        if size is None:
-            raise ValueError('No size found in the file')
-
-        table = []
-        for i, line in enumerate(chain([line], iterator), i):
-            line = line.strip()
-            if not line or line.startswith('#'):
-                continue
-            try:
-                pixel = [float(x) for x in line.split()]
-            except ValueError:
-                raise ValueError("Not a number on line {}".format(i))
-            if len(pixel) != channels:
-                raise ValueError(
-                    "Wrong number of colors on line {}".format(i))
-            table.extend(pixel)
-    finally:
-        if file is not None:
-            file.close()
+    table = lut_to_numpy(lut)
+    table = table.flatten()
 
     instance = cls(size, table, channels=channels,
-                   target_mode=target_mode, _copy_table=False)
+                   target_mode=target_mode, _copy_table=False) #table是size*size*size*3的list
     if name is not None:
         instance.name = name
     return instance
 
 if __name__ == '__main__':
-    lut = load_cube_file('test_lut/ARRI_LogC2Video_Classic709_davinci3d_33.cube')
+    lut = LUT.FromCubeFile('test_lut/ARRI_LogC2Video_Classic709_davinci3d_33.cube')
+    lut = load_lut(lut)
     im = Image.open('test_img/Alexa.bmp')
     im.filter(lut).save('PILTest.bmp')
 
-    #以后要直接对接 IO
